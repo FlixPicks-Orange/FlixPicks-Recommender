926ab7c999023d4894b7bc2613163f5973b6c58f
nvant001 <nvant001@odu.edu>
2024-02-02 09:52:45 -0500

    test coverage for filtering out watched

--- test_app.py
+++ test_app.py
@@ -7,12 +7,27 @@ class TestFilterOutWatched(unittest.TestCase):
         reccomendations = []
         user_titles = ['Movie1', 'Movie2']
         result = filter_out_watched(reccomendations, user_titles)
-        self.assertEqual(result, [])
+        self.assertCountEqual(result, [])
     def test_empty_watchlist(self):
         reccomendations = ['Movie1' , 'Movie2']
         user_titles = []
         result = filter_out_watched(reccomendations, user_titles)
-        self.assertEqual(result, reccomendations)
+        self.assertCountEqual(result, reccomendations)
+    def test_no_common_titles(self):
+        reccomendations = ['Movie1' , 'Movie2']
+        user_titles = ['Movie3']
+        result = filter_out_watched(reccomendations, user_titles)
+        self.assertCountEqual(result, reccomendations)
+    def test_some_common_titles(self):
+        reccomendations = ['Movie1' , 'Movie2']
+        user_titles = ['Movie2','Movie3']
+        result = filter_out_watched(reccomendations, user_titles)
+        self.assertCountEqual(result, ['Movie1'])
+    def test_all_common_titles(self):
+        reccomendations = ['Movie1' , 'Movie2']
+        user_titles = ['Movie1' , 'Movie2']
+        result = filter_out_watched(reccomendations, user_titles)
+        self.assertCountEqual(result, [])
 
 if __name__ == '__main__':
     unittest.main()
